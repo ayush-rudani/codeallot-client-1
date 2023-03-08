74db8565aa9a66c262cf183f7d2dc2f6d12c4ad7
Ayush Rudani <ayushcrudani@gmail.com>
2023-03-09 01:28:59 +0530

    navbar updated

--- codeallot/src/components/Home.js
+++ codeallot/src/components/Home.js
@@ -3,7 +3,7 @@ import CCard from "./CCard";
 function Home() {
     return (
         <>
-            <div class="space-y-3 w-7/12 mt-10 mx-auto mb-8">
+            <div class="space-y-4 w-7/12 mt-10 mx-auto mb-8">
                 <CCard />
                 <CCard />
                 <CCard />
--- codeallot/src/components/Navbar.js
+++ codeallot/src/components/Navbar.js
@@ -21,20 +21,20 @@ export default function NavBar() {
                                 <a href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Explore All Codex</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Your Codex</a>
+                                <a href="/yourcodex" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Your Codex</a>
                             </li>
                         </ul>
 
                         <ul class="flex flex-col p-2 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
                             <li>
-                                <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2 text-center">Sign in</button>
+                                <a type="button" href="/login" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2 text-center">Sign in</a>
                             </li>
 
                             <li>
-                                <button class="inline-flex items-center justify-center p-0.5 mr-1 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                <a href="/register" class="inline-flex items-center justify-center p-0.5 mr-1 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                                     <span class="px-5 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Sign up</span>
-                                </button>
+                                </a>
                             </li>
                         </ul>
                     </div>
