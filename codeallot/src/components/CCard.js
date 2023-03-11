b2d8f1806aec096be34aff1a95930b5ce9058dd0
Ayush Rudani <ayushcrudani@gmail.com>
2023-03-11 14:44:08 +0530

    comp. createcodex is eager to call api

--- codeallot/src/components/CCard.js
+++ codeallot/src/components/CCard.js
@@ -21,7 +21,6 @@ function CCard(props) {
                         <p className="mb-3 text-gray-500 dark:text-gray-400">{props.codex.description}</p>
                         <a href="#" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
                             See Coddex
-                            {/* <svg className="w-6 h-6 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg> */}
                             <FontAwesomeIcon className='w-4 h-4 ml-1' icon={faChevronRight} />
                         </a>
                     </div>
@@ -35,5 +34,7 @@ function CCard(props) {
     )
 }
 
-
 export default CCard;
+
+
+{/* <svg className="w-6 h-6 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg> */ }
--- codeallot/src/components/CreateCodex.js
+++ codeallot/src/components/CreateCodex.js
@@ -1,37 +1,52 @@
-import React from 'react';/*  */
+import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Signup } from './API';
+import toast, { Toaster } from 'react-hot-toast';
 
 function CreateCodex() {
 
+    const [value, setValue] = useState({
+        userid: "",
+        title: "",
+        discription: "",
+        filename: "",
+        content: ""
+    })
 
-    const onChange = React.useCallback((value, viewUpdate) => {
-        console.log('value:', value);
-    }, []);
+    const handleInputs = (e) => {
+        setValue({ ...value, [e.target.name]: e.target.value });
+    }
 
+    const handleSubmit = (e) => {
+
+    }
 
     return (
         <>
-            <div class="w-8/12 rounded-xl mx-auto mt-5 mb-5">
-                <input type="text" class="w-5/12 p-2 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Codex Title" />
+            <div className="w-8/12 rounded-xl mx-auto mt-5 mb-5">
+                <input value={value.title} onChange={handleInputs} type="text" className="w-5/12 p-2 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Codex Title" />
 
-                <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Codex Discription" />
-                <div class="mt-5 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <div class="border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800 py-2">
-                        <ul class="flex flex-wrap text-center">
-                            <li class="mr-2 w-60">
-                                <input type="email" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-2" placeholder="Filename including extension..." />
+                <input value={value.discription} onChange={handleInputs} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Codex Discription" />
+                <div className="mt-5 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800 py-2">
+                        <ul className="flex flex-wrap text-center">
+                            <li className="mr-2 w-60">
+                                <input value={value.filename} onChange={handleInputs} type="text" id="helper-text" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-2" placeholder="Filename including extension..." />
                             </li>
                         </ul>
                     </div>
-                    <CodeMirror
-                        value="console.log('hello world!');"
+
+                    <CodeMirror onChange={handleInputs}
+                        value={value.content}
                         height="500px"
-                        onChange={onChange}
                     />
+
                 </div>
 
-                <div class="mt-4 flex flex-row-reverse">
-                    <button type="button" class="mb-2 px-5 py-2.5 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm text-center">Create Codex</button>
+                <div className="mt-4 flex flex-row-reverse">
+                    <button onClick={handleSubmit} type="button" className="mb-2 px-5 py-2.5 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm text-center">Create Codex</button>
                 </div>
 
             </div>
