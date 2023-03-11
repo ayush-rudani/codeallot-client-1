de3d806d11360d5aa20449864356cb4a72462d52
Ayush Rudani <ayushcrudani@gmail.com>
2023-03-11 08:43:15 +0530

    axios added and M Home comp.

--- codeallot/src/components/Home.js
+++ codeallot/src/components/Home.js
@@ -1,15 +1,70 @@
 import CCard from "./CCard";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
-function Home() {
+import { InfinitySpin } from 'react-loader-spinner';
+
+function Home(props) {
+
+    const [codex, setCodex] = useState();
+    const [loading, setLoading] = useState(true);
+
+    async function getCodex() {
+        const response = await axios.get('api/Codex', {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(response.data)
+        setCodex(response.data);
+        setLoading(false);
+
+        // axios.get("/api/Codex/", {
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // }).then((response) => {
+        //     console.log(response.data);
+        // });
+    }
+
+    useEffect(() => {
+        getCodex();
+    }, []);
+
     return (
         <>
-            <div class="space-y-4 w-7/12 mt-10 mx-auto mb-8">
+
+
+
+            {loading ? (<div className="flex justify-center items-center h-screen"><InfinitySpin width='200' color="#4fa94d" /></div>) :
+                (<div class="space-y-4 w-7/12 mt-10 mx-auto mb-8">
+                    {codex.map((item) => (<CCard key={item.id} id={item.id} title={item.title} description={item.description} />))}
+                </div>)
+            }
+
+            {/* <div class="space-y-4 w-7/12 mt-10 mx-auto mb-8">
+                {!loading ? (
+                    codex.length > 0 ? (
+                        codex.map((item) => (
+                            <CCard key={item.id} id={item.id} title={item.title} description={item.description} />
+                        ))
+
+                    ) : ('No Codex Found')
+                ) : (<InfinitySpin />)
+                }
+
+            </div> */}
+
+
+            {/* <div class="space-y-4 w-7/12 mt-10 mx-auto mb-8">
                 <CCard />
                 <CCard />
                 <CCard />
-            </div>
+            </div> */}
         </>
     );
 }
 
 export default Home;
+
